2d24b0eaeaa521d8d02fd64f73b8d2b21b6df844
epok75 <a.raulet93@gmail.com>
2012-12-18 17:49:36 +0100

    v0.1class views
    Smarty / twig / php

--- app/components/views.class.php
+++ app/components/views.class.php
@@ -1,45 +1,113 @@
 <?php
 /**
- * Allow compatibility between differents template engine : one methode for all
+ * Allow compatibility between differents template engines : one methode for all
+ * This is time for more pleasure and more boobs! :-)
  */
 
 class Render {
 	
-	private $tpl;
-	private $render;
-
-	public function __construct($tpl){
+	private static $instance;
+	// default path for the views in smarty and twig
+	private static $path;
+	// some defaults settings
+	private $tpl 	= 'php';
+	private $ext 	= 'php';
+	private $cache 	= false;
+	// array that store object of either smarty or twig
+	private $renderedPage = array();
+	
+	private function __construct($tpl){
 		
 		switch ($tpl) {
-				
+			
 			case 'twig':
-				require_once ROOT.'/vendors/Twig-1.11.1/lib/Twig/Autoloader.php';
-				Twig_Autoloader::register();
-				$loader = new Twig_Loader_String();
-				$this->render = new Twig_Environment($loader);
-				$this->tpl = 'twig';
+				if (file_exists(ROOT.'/vendors/Twig-1.11.1/lib/Twig/Autoloader.php')) {
+					require_once ROOT.'/vendors/Twig-1.11.1/lib/Twig/Autoloader.php';
+					Twig_Autoloader::register();
+					$loader = new Twig_Loader_Filesystem(self::$path);
+					$this->renderedPage = new Twig_Environment($loader);
+					$this->tpl = 'twig';
+					$this->ext = 'html.twig';
+				} else {
+					die('Failed loaded Twig');
+				}
 				break;
 
 			case 'smarty':
-				require ROOT.'/vendors/Smarty-3.1.12/Smarty.class.php';
-				$this->render = new Smarty();
-				$this->tpl = 'smarty';
-				break;
+				if (file_exists(ROOT.'/vendors/Smarty-3.1.12.2/libs/Smarty.class.php')) {
+					require_once ROOT.'/vendors/Smarty-3.1.12.2/libs/Smarty.class.php';
+					$this->renderedPage = new Smarty();
+					$this->renderedPage->setTemplateDir(self::$path);
+					$this->tpl = 'smarty';
+					$this->ext = 'tpl';
+				} else {
+					die('Failed loaded smarty');
+				}
+				break;				
 		
 			default:
-				$this->tpl = $tpl;
+				$this->ext = $this->tpl = 'php';
 				break;
 		}
 	}
 
-	public function run($file , array $prop) {
-		if($this->tpl === 'twig') {
-			return $this->render->render($file, $prop);
-		} elseif ($this->tpl === 'smarty') {
-			$this->tpl ->assign($prop);
-			return $this->tpl ->display($file);
+	// singleton
+	public static function getInstance($tpl) {
+		if (!is_null(self::$instance)) {
+			return self::$instance;
 		} else {
-			return require $file;
+			// set the path for the view
+			self::$path = ROOT.'/src/';
+			self::$instance = new Render($tpl);
+			return self::$instance;
 		}
 	}
-}
+
+	public function run($file , array $prop = null) {
+		// check if the file exist
+		$this->fileExists($file);
+
+		//return the template depending of the engine chosen by the user
+		if($this->tpl === 'twig') {
+			// display twig template
+			print $this->renderedPage->render($file.'.html.twig', $prop);
+		} elseif ($this->tpl === 'smarty') {
+			// check if there are arguments pass to the method to avoid bug
+			if ($prop !== null) {
+				$this->renderedPage ->assign($prop);	
+			}
+			// display smarty template
+			$this->renderedPage ->display($file.'.tpl');
+		} else {
+			// if there is no template engine chosen, just make a require
+			return require self::$path.$file.'.php';
+		}
+	}
+	
+	// check if the file pass through the method exist
+	private function fileExists($file) {
+		if (file_exists(self::$path.$file.'.'.$this->ext)) {
+			return true;
+		} else {
+			die('failed to load the file, check the path');
+		}
+	}
+
+	// setter ; set the cache if it's true on config.yml
+	/*public static function setCache($bool){
+		die('ewdwdewed');
+		$this->$cache = $bool;
+		$this->renderedPage->setCacheDir(ROOT.'/app/cache');
+	}*/
+
+	// getter
+	public function getTpl() {
+		return $this->tpl;
+	}
+	public function getRender(){
+		return $this->renderedPage;
+	}
+	public function getPath() {
+		return self::$path;
+	}
+} 
