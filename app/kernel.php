2a0bca1b5a58d860d3f1fd05502844286e820d54
Clement Seiller <clement.seiller@supinternet.fr>
2012-12-19 16:52:09 +0100

    Add include

--- app/kernel.php
+++ app/kernel.php
@@ -1,5 +1,25 @@
 <?php
 define("ROOT", realpath(__DIR__."/../")); // base of the web site
 
-require_once(ROOT.'/app/components/spyc/Spyc.php'); // YAML parser
+// Include of usefull components, do not delete
+require_once(ROOT.'/app/components/spyc/Spyc.php');
+require_once(ROOT.'/app/components/router.class.php');
+require_once(ROOT.'/app/components/sqlConnect.class.php'); 
+
 $conf = Spyc::YAMLLoad(ROOT.'/app/config/config.yml'); // Configuration 
+
+
+/*
+
+	Analyse/interpretation conf object
+
+	Connection bdd
+
+	
+
+*/
+//$routeArray = route::getRoute();
+
+//var_dump($routeArray);
+
+var_dump($conf);
