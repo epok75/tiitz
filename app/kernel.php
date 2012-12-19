2a3e3520e781a71f2a17bfdd843854c18b2fe04f
teixeira-c <cyril.teixeira@supinternet.fr>
2012-12-19 14:31:56 +0100

    ad kernel + index

---
+++ app/kernel.php
@@ -0,0 +1,5 @@
+<?php
+
+    define("ROOT", realpath(__DIR__.'..\\..\\')); // base of the web site
+    require_once(ROOT.'/app/components/spyc/Spyc.php'); // YAML parser
+    $conf = Spyc::YAMLLoad(ROOT.'/app/config/config.yml'); // Configuration 
