6f679fe7d721ecbcfbd6846bea061c0f796cfbe8
Romain Reynaud <romr09@gmail.com>
2013-01-02 15:17:52 +0100

    Style

--- app/gui/views/index.php
+++ app/gui/views/index.php
@@ -13,53 +13,52 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 </head>					 
 <body>
-	<header>
-		Configuration initiale
-	</header>
-	<div>
-		<div class="content">
-			<div class="formPart">
-				<h4>Base de données</h4>
-				<form method="post" action="<?php $_SERVER["SCRIPT_NAME"] ?>">
-						<label for="user">Utilisateur : </label>
-						<input type="text" name="user" onblur="checkBDD()" id="user" placeholder="root" />
-						<label for="pwd">Mot de Passe : </label>
-						<input type="password" onblur="checkBDD()" name="pwd" id="pwd" />
-						<label for="adress">Hôte : </label>
-						<input type="text" name="adress" id="adress" onblur="checkBDD()" placeholder="127.0.0.1"/>
-						<label for="name">Nom : </label>
-						<input type="text" name="name" id="name" onblur="checkBDD()" placeholder="tiitzBDD" />
-			</div>
-			<div class="notif"><?php if (isset($_GET["err"]) && $_GET["err"] == "conn") echo "Problème de connexion à la BDD"; else if (isset($_GET["err"]) && $_GET["err"] == "name") echo "La BDD choisie n'existe pas !"; else if (isset($_GET["err"]) && $_GET["err"] == "bundle") echo "Erreur lors de la création du bundle"; ?></div>
-			<div class="formPart">
-				<h4>Moteur de templates</h4>
-					<label for="tpl">Choix : </label>
-					<select id="tpl" name="tpl">
-						<option value="twig">Twig</option>
-						<option value="smarty">Smarty</option>
-						<option value="php">Aucun</option>
-					</select>
-			</div>
-			<div class="formPart">
-				<h4>Langage de Routing</h4>
-					<label for="routesLang">Langage : </label>
-						<select id="routesLang" name="routesLang">
-							<option value="yml">YAML</option>
-							<option value="php">PHP</option>
+		<fieldset>
+			<header>
+				Configuration initiale
+			</header>
+			<div class="content">
+				<div id="notif" class="error">&nbsp;</div>
+				<div class="formPart">
+					<h4>Base de données</h4>
+					<form method="post" action="<?php $_SERVER["SCRIPT_NAME"] ?>">
+							<label for="user">Utilisateur : </label>
+							<input type="text" name="user" onblur="checkBDD()" id="user" placeholder="root" />
+							<label for="pwd">Mot de Passe : </label>
+							<input type="password" onblur="checkBDD()" name="pwd" id="pwd" />
+							<label for="adress">Hôte : </label>
+							<input type="text" name="adress" id="adress" onblur="checkBDD()" placeholder="127.0.0.1"/>
+							<label for="name">Nom : </label>
+							<input type="text" name="name" id="name" onblur="checkBDD()" placeholder="tiitzBDD" />
+				</div>
+				<div class="formPart">
+					<h4>Moteur de templates</h4>
+						<label for="tpl">Choix : </label>
+						<select id="tpl" name="tpl">
+							<option value="twig">Twig</option>
+							<option value="smarty">Smarty</option>
+							<option value="php">Aucun</option>
 						</select>
-			</div>
-			<!--<div class="formPart">
-				<h4>Nom du premier bundle</h4>
-					<label for="bundle">Nom du bundle : </label>
-					<input type="text" id="bundle" name="bundle"/>
-			</div>-->
-			<div class="formPart">
-				<input type="submit" value="Terminer" name="firstConfig" class="btn btn-large btn-primary" />
-			</div>
-			<span class="formEnd"></span>
-			</form>
+				</div>
+				<div class="formPart">
+					<h4>Langage de Routing</h4>
+						<label for="routesLang">Langage : </label>
+							<select id="routesLang" name="routesLang">
+								<option value="yml">YAML</option>
+								<option value="php">PHP</option>
+							</select>
+				</div>
+				<!--<div class="formPart">
+					<h4>Nom du premier bundle</h4>
+						<label for="bundle">Nom du bundle : </label>
+						<input type="text" id="bundle" name="bundle"/>
+				</div>-->
+				<div class="formEnd">
+					<input type="submit" value="Terminer" name="firstConfig" class="btn btn-large btn-primary" />
+				</div>
+				</form>
 		</div>
-	</div>
+		</fieldset>
 	<script type="text/javascript" src="../gui/js/guiJS.js"></script>
 </body>
 </html>
--- web/gui/js/guiJS.js
+++ web/gui/js/guiJS.js
@@ -12,7 +12,18 @@ function checkBDD ()
    				type: "POST",
    				url: "../gui/js/checkBDD.php",
    				data: "name="+name+"&user="+user+"&pwd="+pwd+"&adress="+adress,
-   				success: function(msg){ alert(msg); }
+   				success: function(msg){
+   					if (msg != "")
+   					{
+   						$('#notif').attr('class', "error");
+   						$('#notif').html(msg);
+   					}
+   					else
+   					{
+   						$('#notif').attr('class', "success");
+   						$('#notif').html("La base de donnée à été trouvée");
+   					}
+   				}
  			});
  		}
 }
