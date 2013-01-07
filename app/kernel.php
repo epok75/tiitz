ef450a3bdeaba2fea0a3f0b1184f9db375031342
epok75 <a.raulet93@gmail.com>
2013-01-07 20:06:07 +0100

    Dev Toolbar v.01
    Design and few functionnalities

--- app/kernel.php
+++ app/kernel.php
@@ -3,17 +3,11 @@ define("ROOT", realpath(__DIR__."/../")); // base of the web site
 define("SRC", realpath(ROOT."/src"));
 define("GUI", realpath(ROOT."/app/gui/")); // base of the GUI
 
+
 // Test error
 require_once(ROOT.'/app/components/error/ErrorCore.class.php');
 require_once(ROOT.'/app/components/error/ErrorExtend.class.php');
 $error = new ErrorExtend(3);
-
-print '<pre>';
-//var_dump($error -> exportArray());
-//var_dump($error -> exportJson());
-print '</pre>';
-//include ('test.php');
-
 
 
 // Include YAML parsing tool
@@ -21,11 +15,11 @@ require_once(ROOT.'/app/components/spyc/Spyc.php');
 $comp = Spyc::YAMLLoad(ROOT.'/app/config/components.yml');
 $conf = Spyc::YAMLLoad(ROOT.'/app/config/config.yml');
 
-
 // Include the components contains in components.yml
 foreach ($comp as $k => $v) {
 	require_once(ROOT.$v);
 }
+
 
 if (!empty($conf["existingproject"]) && $conf["existingproject"] === true)
 {
@@ -38,21 +32,14 @@ if (!empty($conf["existingproject"]) && $conf["existingproject"] === true)
 	var_dump($route);
 	if (is_file(ROOT.$route["path"])) {
 		require_once ROOT.$route["path"];
-	}
+	} 
 	else
 		echo "Page 404";
 }
 else
 	require_once GUI."\\index.php";
 
-
-/*$routeArray = route::getRoute();
-
-var_dump($routeArray);*/
-
-//var_dump($conf);
-
-require_once("../app/components/views.class.php");
-require_once("../app/components/validator.class.php");
-require_once(ROOT.'/app/components/spyc/Spyc.php'); // YAML parser
-$conf = Spyc::YAMLLoad(ROOT.'/app/config/config.yml'); // Configuration 
+// toolbar for development environment
+if($conf['environnement'] == 'dev') {
+	devToolbar::toolbar($conf['environnement'], $route);
+}
