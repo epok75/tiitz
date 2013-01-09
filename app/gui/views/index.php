b603df84e320672c825d54797163f196a9d2a39a
Romain Reynaud <romr09@gmail.com>
2013-01-09 18:44:54 +0100

    je ne sais plus ce que j'ai fais sur celui là
    ça doit être un espace supprimé ou un truc comme ça

--- app/gui/views/index.php
+++ app/gui/views/index.php
@@ -4,9 +4,9 @@
 	<meta charset="utf-8">
 	<title>TiiTz Framework</title>
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
-	<link href="tiitz/css/bootstrap.css" rel="stylesheet" />
-    <link href="tiitz/css/bootstrap-responsive.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="tiitz/css/style-gui.css">
+	<link href="tiitz/css/bootstrap.css" rel="stylesheet" type='text/css'/>
+    <link href="tiitz/css/bootstrap-responsive.css" rel="stylesheet" type='text/css' />
+    <link rel="stylesheet" type="text/css" href="tiitz/css/style-gui.css" type='text/css' />
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 </head>					 
