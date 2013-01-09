b1f6e47f9fc86eb0bb996207844cdd674df971e2
Clement Seiller <clement.seiller@supinternet.fr>
2013-01-09 16:03:42 +0100

    Requirements: Regexp, int, string, post, get

--- app/components/router.class.php
+++ app/components/router.class.php
@@ -4,6 +4,50 @@ class Route {
 
 	private static $arrayRoute;
 
+	private static function checkRequirement($req, $params) {
+
+		$valid = array();
+		$match = array();
+		foreach ($params as $k => $v) {
+
+			if (array_key_exists($params[$k]["name"], $req))
+			{
+				if ($req[$params[$k]["name"]] == "int") {
+					$valid['int'] = (is_int($params[$k]["value"])) ? true : false;
+				}
+				else if ($req[$params[$k]["name"]] == "string") {
+					$valid['string'] = (is_string($params[$k]["value"])) ? true : false;
+				}
+				else
+				{
+					if (preg_match("/".$req[$params[$k]["name"]]."/", $params[$k]["value"]))
+						$match[$params[$k]["name"]] = true;
+					else
+						$match[$params[$k]["name"]] = false;
+				}
+			}
+
+			if (array_key_exists("_method", $req))
+			{
+				if ($req["_method"] == "post" || $req["_method"] == "POST")
+					$valid['post'] = (!empty($_POST)) ? true : false;
+
+				if ($req["_method"] == "get" || $req["_method"] == "GET")
+					$valid['get'] = (!empty($_GET)) ? true : false;
+			}
+		}
+
+		foreach ($match as $value) {
+			if ($value === false)
+				$valid["regexp"] = false;
+		}
+		foreach ($valid as $value) {
+			if ($value === false)
+				return false;
+		}
+		return true;
+	}
+
 	private static function deleteEmptyValues(array $array) {
 		#var_dump($array);
 		$arrayReturn = array();
@@ -25,11 +69,14 @@ class Route {
 			die('Routing file missing');
 		$arraySubRoutes = Spyc::YAMLLoad(ROOT.$arrayRoutes[$type]['ressource']);
 
+		#echo "ROUTE : ";var_dump($arraySubRoutes);echo "--------------<br />";
+
 		foreach ($arraySubRoutes as $key => $params) {
 			$arraySubRoutes[$key]['pattern'] = self::deleteEmptyValues(explode('/', $arraySubRoutes[$key]['pattern']));
 			//var_dump($arraySubRoutes[$key]['pattern']);
 			$arraySubRoutes[$key]['type'] = $type;
 			$arraySubRoutes[$key]['params'] = array();
+
 			foreach ($arraySubRoutes[$key]['pattern'] as $_key => $value) {
 				if(strpos($value, '{') === 0 && strpos($value , '}') === strlen($value) -1 ) {
 					$value = str_replace('{', '', $value);
@@ -41,8 +88,16 @@ class Route {
 				}
 
 			}
-			#var_dump($arraySubRoutes[$key]);
-			if($arraySubRoutes[$key]['pattern'] == $actualRoute)
+			#echo "CHOSEN ROUTE : ";var_dump($arraySubRoutes[$key]);echo "--------------<br />";
+
+			/*********************
+				Code Requirements
+			*********************/
+			$r = true;
+			if (!empty($arraySubRoutes[$key]['requirements']))
+				$r = self::checkRequirement($arraySubRoutes[$key]['requirements'], $arraySubRoutes[$key]['params']);
+
+			if($arraySubRoutes[$key]['pattern'] == $actualRoute && $r)
 				return $arraySubRoutes[$key];
 		}
 		return false;
@@ -61,7 +116,9 @@ class Route {
 		#echo 'YAML SRC :';var_dump($yaml);echo '---------<br />';
 
 		$selectedRoute = self::parseRoutes($yaml, $urlParams);
-                var_dump($selectedRoute);
+
+        #echo "SELECTED ROUTE : ";var_dump($selectedRoute);echo "--------------<br />";
+
 		if($selectedRoute){
 			$arrayController = explode(':', $selectedRoute['defaults']['_controller']);
 			if(count($arrayController) !== 2){
@@ -83,5 +140,3 @@ class Route {
 		return self::$arrayRoute;
 	}
 }
-
-
