e79749211369d5892b0fdf6775298f4d91c6eabf
Ben <debernardi.benjamin@gmail.com>
2013-01-10 14:52:07 +0100

    TiiTz & tzController classes

---
+++ app/components/tiitz.php
@@ -0,0 +1,34 @@
+<?php
+
+class TiiTz {
+    
+    public $tzView;
+    
+    public $tzSQL;
+    
+    public $tzPlugin;
+    
+    public $tzValidator;
+    
+    
+    public function __construct($tzView, $tzPlugin, $tzValidator, $tzError, $tzSQL = null) {
+        if($tzView !instanceof TzView) {
+            
+        }
+        elseif($tzPlugin !instanceof TzPlugin) {
+            
+        }
+        elseif($tzValidator !instanceof TzValidator) {
+            
+        }
+        elseif($tzError !instanceof TzError) {
+            
+        }
+        elseif(!is_null($tzSQL) && $tzSQL !instanceof TzPDO){
+            
+        }
+        
+        
+    }
+    
+}
---
+++ app/components/tzController.class.php
@@ -0,0 +1,7 @@
+<?php
+
+
+class tzController extends TiiTz {
+
+	
+}
