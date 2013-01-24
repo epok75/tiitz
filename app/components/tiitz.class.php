a27397017228f60c37e130d8b25412bdbbb54e9b
Clement Seiller <clement.seiller@supinternet.fr>
2013-01-24 14:33:47 +0100

    Switch tiitz class to controller class

--- app/components/tiitz.class.php
+++ app/components/tiitz.class.php
@@ -2,21 +2,5 @@
 
 class TiiTz {
     
-    public $tzRender;
-        
-    public $tzPlugin;
-    
-    public $tzValidator;
-    
-    private static $tiitzVersion = '0.1';
-    
-    public function __construct(tzRender $tzRender) {
-        $this->tzRender = $tzRender;
-        //$this->tzValidator = new tzValidator;
-    }
-
-    public static function getTiitzVersion() {
-        return self::$tiitzVersion;
-    }
    
 }  
--- app/components/tzController.class.php
+++ app/components/tzController.class.php
@@ -1,7 +1,20 @@
 <?php
 
 
-class tzController extends TiiTz {
+class tzController {
 
-	
+	public $tzRender;  
+    public $tzPlugin;
+    public $tzValidator;
+    private static $tiitzVersion = '0.1';
+    
+    public function __construct(tzRender $tzRender) {
+        $this->tzRender = $tzRender;
+        //$this->tzValidator = new tzValidator;
+    }
+
+    public static function getTiitzVersion() {
+        return self::$tiitzVersion;
+    }
+
 }
